42d82be700a4430156770ec9e4fcd40b3c1e2c51
vijayreddy1991 <vijayreddy.chenna@gmail.com>
2019-02-19 11:48:09 +0530

    update ruby latest version for u14ruball image

--- version/2_6.sh
+++ version/2_6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-RUBY_VER=2.6.0
+RUBY_VER=2.6.1
 echo "================= Installing Ruby $RUBY_VER ==================="
 rvm requirements
 rvm install "$RUBY_VER" --disable-binary
@@ -8,4 +8,3 @@ rvm use "$RUBY_VER"
 
 # Install ruby gems
 gem install bundler
-
